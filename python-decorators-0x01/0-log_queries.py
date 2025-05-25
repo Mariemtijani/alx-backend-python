370e0d3f06596b06c9fe727ad0ea51bace041706
Mariem Tijani <mariemtijani60@gmail.com>
2025-05-25 19:38:15 +0100

    first decoration

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,25 @@
+import sqlite3
+import functools
+
+#### decorator to lof SQL queries
+
+""" YOUR CODE GOES HERE"""
+
+def log_queries(func):
+    def wrapper():
+       print (f"Start execute : {func.__name__}")
+       func()
+       print("Done")
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
