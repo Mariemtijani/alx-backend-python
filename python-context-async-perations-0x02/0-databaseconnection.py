1e4ee5a327113202b2dca6eb98b6b0b7256e6ff4
Mariem Tijani <mariemtijani60@gmail.com>
2025-06-13 14:26:58 +0100

    contect manaer

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -12,3 +12,15 @@ class DatabaseConnection:
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.connection:
             self.connection.close()
+
+
+if __name__ == "__main__":
+    db_file = "my_database.db"  
+
+    with DatabaseConnection(db_file) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users")
+        rows = cursor.fetchall()
+
+        for row in rows:
+            print(row)
