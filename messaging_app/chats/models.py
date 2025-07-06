07d1db79e526e3c7f92dbfe747ae61d1425ff6e8
Mariem Tijani <mariemtijani60@gmail.com>
2025-07-06 01:11:39 +0100

    Refactor User, Conversation, and Message models for clarity and structure

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -3,11 +3,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
-    USERNAME_FIELD = 'username'  # or 'email' if you're using email login
-    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+    USERNAME_FIELD = 'username' 
+    password = models.CharField(max_length=128)
 
     def __str__(self):
         return self.username
@@ -15,8 +15,8 @@ class User(AbstractUser):
 
 class Conversation(models.Model):
     conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    # participants = models.ManyToManyField(User, related_name='chats')
-    # created_at = models.DateTimeField(auto_now_add=True)
+    participants = models.ManyToManyField(User, related_name='chats')
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Conversation {self.conversation_id}"
@@ -24,8 +24,8 @@ class Conversation(models.Model):
 
 class Message(models.Model):
     message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    # sender = models.ForeignKey(User, on_delete=models.CASCADE)
-    # conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
     message_body = models.TextField()
     sent_at = models.DateTimeField(auto_now_add=True)
 
