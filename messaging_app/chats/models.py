7151c088eae5b812d410caf5a35978500a285b5a
Mariem Tijani <mariemtijani60@gmail.com>
2025-07-06 01:00:42 +0100

    Refactor User, Conversation, and Message models for improved structure and clarity

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,25 +1,33 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Custom user model
 class User(AbstractUser):
-    
-    pass
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    USERNAME_FIELD = 'username'  # or 'email' if you're using email login
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.username
 
 
 class Conversation(models.Model):
-    participants = models.ManyToManyField(User, related_name='conversations')
-    created_at = models.DateTimeField(auto_now_add=True)
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # participants = models.ManyToManyField(User, related_name='chats')
+    # created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Conversation {self.id}"
+        return f"Conversation {self.conversation_id}"
 
 
 class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
-    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    # conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"From {self.sender.username} at {self.timestamp}"
+        return f"Message from {self.sender.username} at {self.sent_at}"
